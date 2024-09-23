e9cbbc94b174a9e6606ed3f927f75ed1bc73647c
Irina Vikhrova <ivikhrova@optimalgroup.ru>
2024-09-23 16:52:38 +0300

    dedicate a branch for a dist

--- components/kanban/useKanbanQuery.ts
+++ components/kanban/useKanbanQuery.ts
@@ -2,6 +2,7 @@ import {useQuery} from '@tanstack/vue-query'
 import {DB_ID, COLLECTION_DEALS} from "~/app.constants";
 import {KANBAN_DATA} from "~/components/kanban/kanban.data";
 import type {IDeal} from "~/types/deals.types";
+import type { IColumn } from './kanban.types'
 
 export function useKanbanQuery() {
   return useQuery({
@@ -17,7 +18,10 @@ export function useKanbanQuery() {
       }
       },
     select(data) {
-      const newBoard = [...KANBAN_DATA]
+      const newBoard: IColumn[] = KANBAN_DATA.map(column => ({
+        ...column,
+        items: [],
+      }))
       const deals = data.documents as unknown as IDeal[]
 
       for (const deal of deals) {
